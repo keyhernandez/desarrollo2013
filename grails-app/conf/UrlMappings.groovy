1570ea907bd7f2272dd469a8dab41b4e2cceb426
Susana Jeronimo <susy.jm@gmail.com>
2013-06-29 01:28:59 -0430

    [ADD] Agregando las lecturas con un simulador JSON

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -22,6 +22,12 @@ class UrlMappings {
                 
          "/rest/test/"(controller:"BusquedaProductos", action:"test")
         "/rest/consumirServicio/"(controller:"BusquedaProductos", action:"consumirServicio")
+
+        // lectura del simulador
+        "/rest/json_test/"(controller:"Prorest", action:"json_test")
+        "/rest/productos_rest/"(controller:"Prorest", action:"productos_rest")
+        "/rest/productos_rest_id/$id?"(controller:"Prorest", action:"productos_rest_id")
+
 		"/"(view:"/index")
 		"500"(view:'/error')
         "/"(controller:'index',action:'index')
